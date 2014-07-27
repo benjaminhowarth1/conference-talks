c5e83638d309e4f57d1af78fc417b5fe89829847
benjamin.howarth <benjamin@codegecko.com>
2014-07-27 01:23:28 +0100

    Updated with a refactored project. Routes don't currently work in Angular - to fix (refactored to use $q's promises).

---
+++ RomanSPA/Demos/RomanSPA.Demo.AngularJS/App/app.js
@@ -0,0 +1,39 @@
+﻿angular.module('RomanSPA', ['ngRoute'])
+        .config(['$routeProvider', '$locationProvider', '$httpProvider', function
+                 ($routeProvider, $locationProvider, $httpProvider) {
+
+            $locationProvider.html5Mode(true);
+
+            $httpProvider.defaults.headers.get = { 'X-RomanViewRequest': 'true' };
+
+            var populateRouteTable = function (data) {
+                for (var r = 0; r < data.length; r++) {
+                    $routeProvider.when(data[r].RoutePattern, data[r].controller, data[r].templatePath)
+                }
+            }
+
+            var getRoutes = function (routeApi) {
+                var defer = $q.defer();
+                $.ajax({
+                    url: routeApi,
+                    success: function (data) { defer.resolve(data); },
+                    fail: function (data) { defer.reject(data); }
+                });
+                return defer.promise;
+            }
+
+            var getRouteTemplate = function ($routeProvider, pattern, controller, templUrl) {
+                $httpProvider.get(templUrl)
+                     .success(function (data) {
+                        setRoute($routeProvider, pattern, controller, data);
+                        $routeProvider.otherwise({ redirectTo: '/', caseInsensitiveMatch: true });
+                     });
+            }
+
+            var routes = getRoutes('/api/Angular/Routes');
+            routes.then(populateRouteTable);
+        }])
+    .run(['$route', function ($route) {
+        if ($route.routes.length > 0) $route.reload();
+    }])
+    .value('breeze', window.breeze);
---
+++ RomanSPA/RomanSPA.AngularJS/app/app.js
@@ -0,0 +1,45 @@
+﻿angular.module('RomanSPA', ['ngRoute'])
+        .config(['$routeProvider', '$locationProvider', '$q', function 
+                 ($routeProvider, $locationProvider, $q) {
+
+            $locationProvider.html5Mode(true);
+
+            $http.defaults.headers.get['X-RomanViewRequest'] = 'true';
+
+            var populateRouteTable = function (data) {
+                for (var r = 0; r < data.length; r++) {
+                    $routeProvider.when(data[r].RoutePattern, data[r].controller, data[r].templatePath)
+                }
+            }
+
+            var getRoutes = function (routeApi) {
+                var defer = $q.defer();
+                $.ajax({
+                    url: routeApi,
+                    success: function (data) {
+                        defer.success(data);
+                    },
+                    fail: function (data) {
+                        defer.fail(data);
+                    }
+                });
+                return defer.promise;
+            }
+
+            var getRouteTemplate = function ($routeProvider, pattern, controller, templUrl) {
+                $.ajax({ url: templUrl, beforeSend: addHeaders, async: false })
+                 .done(function (data) {
+                     setRoute($routeProvider, pattern, controller, data);
+                     $routeProvider.otherwise({ redirectTo: '/', caseInsensitiveMatch: true });
+                 });
+            }
+
+            var routes = getRoutes('/api/Angular/Routes');
+            routes.then(populateRouteTable);
+
+
+        }])
+    .run(['$route', function ($route) {
+        if ($route.routes.length > 0) $route.reload();
+    }])
+    .value('breeze', window.breeze);
